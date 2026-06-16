835ec8382a9d96dfaac4abe8d01ff96e2d52bf27
Jose Rojas <114960950+fenixreds@users.noreply.github.com>
2026-06-16 15:15:04 -0500

    feat(middleware): add global error handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { errorHandler } = require('./middlewares/error.middleware');
+const { success } = require('./utils/response');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+app.use(errorHandler);
 
 module.exports = app;
