3a3b1aecffef54df7682e901e06038e3bc35d256
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2026-06-17 10:32:15 -0300

    docs: add JSDoc documentation to validate middleware with usage example
    Added comprehensive documentation to the `validate` middleware, including a practical example demonstrating how to define a Joi schema and apply it to a route. This improves developer onboarding and clarifies middleware usage.

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,5 +1,22 @@
 const Joi = require('joi');
 
+/**
+ * Middleware para validar el cuerpo de la solicitud usando un esquema Joi.
+ * 
+ * Ejemplo de uso:
+ * 
+ * const Joi = require('joi');
+ * const { validate } = require('../middlewares/validate.middleware');
+ * 
+ * const userSchema = Joi.object({
+ *   name: Joi.string().required(),
+ *   email: Joi.string().email().required(),
+ * });
+ * 
+ * router.post('/users', validate(userSchema), (req, res) => {
+ *   res.send('User validated!');
+ * });
+ */
 const validate = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
