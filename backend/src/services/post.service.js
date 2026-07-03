5f4944d61954bc554758d79fea8271ef92204278
Lazzariniiris <lazzariniiris@gmail.com>
2026-07-02 21:37:52 -0300

    feat: add category badges to post cards

--- backend/src/services/post.service.js
+++ backend/src/services/post.service.js
@@ -22,6 +22,13 @@ const getAllPosts = async ({ categorySlug } = {}) => {
           email: true,
         },
       },
+      categories: {
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+        },
+      },
     },
     orderBy: {
       createdAt: 'desc',
