43db641cab48dcf1a623dbab4bd4ef9ce3d0a673
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2026-07-10 09:49:23 -0300

    feat: endpoint GET /api/posts/:postId/comments

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { create } = require('../controllers/comment.controller');
+const { create, getByPost } = require('../controllers/comment.controller');
 const { authMiddleware } = require('../middlewares/auth.middleware');
 const { validate } = require('../middlewares/validate.middleware');
 const { createCommentSchema } = require('../middlewares/schemas');
 
 const router = Router();
 
+router.get('/:postId/comments', getByPost);
 router.post('/:postId/comments', authMiddleware, validate(createCommentSchema), create);
 
 module.exports = router;
