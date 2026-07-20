b86bd97167ffa20d92bc2952cdfff150a5091427
Lazzariniiris <lazzariniiris@gmail.com>
2026-07-20 14:03:05 -0300

    feat: integrar búsqueda de posts en home

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -4,3 +4,4 @@ export { default as EmptyState } from './EmptyState';
 export { default as ConfirmModal } from './ConfirmModal';
 export { default as ImageUpload } from './ImageUpload';
 export { default as Pagination } from './Pagination';
+export { default as SearchInput } from './SearchInput';
